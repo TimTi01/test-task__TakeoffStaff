d51980991640b280e2a3b68a21e32f6b2b3e5fe1
TimTi01 <TimF1234@yandex.ru>
2022-05-13 16:34:39 +0300

    added logout

--- src/Components/Bar.tsx
+++ src/Components/Bar.tsx
@@ -1,15 +1,38 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { setAuthUser } from '../features/authSlice';
+import { useAppDispatch, useAppSelector } from '../hooks/redux';
 
 export default function Bar() {
+  const auth = useAppSelector(state => state.authReducer.authorization)
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!auth) {
+      navigate('/')
+    }
+  }, [auth])
+
+  console.log('auth', auth);
+
   return (
     <AppBar position='fixed'>
         <Toolbar>
-        <Typography variant="h6" 
-                    component="div" 
-                    sx={{ flexGrow: 1 }}
-        >
-            Takeoff Staff
-        </Typography>
+          <Typography variant="h6" 
+                      component="div" 
+                      sx={{ flexGrow: 1 }}
+          >
+              Takeoff Staff
+          </Typography>
+          {
+            auth && <Button color="inherit"
+                            onClick={() => dispatch(setAuthUser(false))}
+                    >
+                      Logout
+                    </Button> 
+          }
         </Toolbar>
     </AppBar>
   )
